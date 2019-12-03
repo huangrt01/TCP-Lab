d59805730f4b2ee74aa78d1662982934c4f5fa89
Keith Winstein <keithw@cs.stanford.edu>
2019-12-03 03:17:51 -0800

    CS144 Lab 6 assignment

--- libsponge/util/address.cc
+++ libsponge/util/address.cc
@@ -116,6 +116,14 @@ uint32_t Address::ipv4_numeric() const {
     return be32toh(ipv4_addr.sin_addr.s_addr);
 }
 
+Address Address::from_ipv4_numeric(const uint32_t ip_address) {
+    sockaddr_in ipv4_addr{};
+    ipv4_addr.sin_family = AF_INET;
+    ipv4_addr.sin_addr.s_addr = htobe32(ip_address);
+
+    return {reinterpret_cast<sockaddr *>(&ipv4_addr), sizeof(ipv4_addr)};
+}
+
 // equality
 bool Address::operator==(const Address &other) const {
     if (_size != other._size) {
